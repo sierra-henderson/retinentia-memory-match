2942da12d5795656cd532a88e7d433d864c14bbc
Sierra Henderson <henderson.sierra@yahoo.com>
2020-04-24 17:18:20 -0700

    New theme, ability to choose level, loading screen, timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,51 @@ var firstCardClasses = null;
 var secondCardClasses = null;
 var maxMatches = 9;
 var matches = 0;
+var container = document.querySelector('.container')
 var modalOverlay = document.querySelector('.modal-overlay');
+var modalContent = document.querySelector('.modal-content h2');
 var attempts = 0;
 var gamesPlayed = 0;
 var gamesPlayedElement = document.getElementById('gamesPlayed');
 var attemptsElement= document.getElementById('attempts');
 var accuracyElement = document.getElementById('accuracy');
+var buttonContainer = document.querySelector('.button-container')
+var normalButton = document.getElementById('normal');
+var expertButton = document.getElementById('expert');
 var modalButton = document.getElementById('modalButton');
-var classNames = ['js-logo', 'js-logo', 'css-logo', 'css-logo', 'docker-logo', 'docker-logo', 'github-logo', 'github-logo', 'html-logo', 'html-logo', 'mysql-logo', 'mysql-logo', 'node-logo', 'node-logo', 'php-logo', 'php-logo', 'react-logo', 'react-logo'];
+var modalParagraph = document.getElementById('modalParagraph')
+var timeLeft = document.getElementById('timeLeft')
+var classNames = ['cauldron', 'cauldron', 'crystal', 'crystal', 'flowers', 'flowers', 'lavender', 'lavender', 'mushroom', 'mushroom', 'potion', 'potion', 'runes', 'runes', 'salt', 'salt', 'skull', 'skull']
 var cardFront = document.querySelectorAll('.card-front');
+var num;
+var id = setInterval(countdown, 100);
 
-shuffleClassNames();
-createCards();
+function init() {
+  shuffleClassNames();
+  createCards();
+  timeLeft.textContent = (num / 10).toFixed(1) + "s";
+  container.classList.remove('hidden');
+  modalOverlay.classList.add('hidden');
+}
+
+normalButton.addEventListener('click', function() {
+  if (container.className.includes('hidden')) {
+    num = 600;
+    init();
+  } else {
+    resetGame(600);
+  }
+});
+
+expertButton.addEventListener('click', function () {
+  if (container.className.includes('hidden')) {
+    num = 400;
+    init();
+  } else {
+    resetGame(400);
+  }
+});
+
 
 gameCards.addEventListener('click', handleClick);
 
@@ -27,22 +60,29 @@ function handleClick(event) {
   event.target.className += ' hidden';
   if (!firstCardClicked) {
     firstCardClicked = event.target;
-    firstCardClasses = firstCardClicked.previousElementSibling.className;
+    firstCardClasses = firstCardClicked.nextElementSibling.className;
   } else {
     secondCardClicked = event.target;
-    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    secondCardClasses = secondCardClicked.nextElementSibling.className;
     gameCards.removeEventListener("click", handleClick);
     attempts++;
 
     if (firstCardClasses === secondCardClasses){
-      matches++;
-      displayStats();
-      if (matches === maxMatches) {
-        modalOverlay.classList.remove('hidden');
+      if (firstCardClasses.includes('cauldron') && secondCardClasses.includes('cauldron') && matches < 8) {
+        loseGame();
+      } else {
+        matches++;
+        displayStats();
+        if (matches === maxMatches) {
+          clearInterval(id);
+          modalOverlay.classList.remove('hidden');
+          modalContent.textContent = "Congratulations! You've found all of the ingredients";
+          modalParagraph.textContent = "I'd get out of the cottage while you still can..."
+        }
+        gameCards.addEventListener('click', handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
       }
-      gameCards.addEventListener('click', handleClick);
-      firstCardClicked = null;
-      secondCardClicked = null;
     } else {
       setTimeout(function() {
         displayStats();
@@ -69,7 +109,7 @@ function calculateAccuracy(attempts, matches) {
   return Math.trunc(matches / attempts * 100) + "%";
 }
 
-function resetGame() {
+function resetGame(time) {
   attempts = 0;
   matches = 0;
   gamesPlayed++;
@@ -78,6 +118,8 @@ function resetGame() {
   modalOverlay.classList.add('hidden');
   shuffleClassNames();
   createCards();
+  num = time;
+  id = setInterval(countdown, 100);
 }
 
 function resetCards() {
@@ -87,7 +129,6 @@ function resetCards() {
   }
 }
 
-modalButton.addEventListener('click', resetGame);
 
 function shuffleClassNames() {
   for (var i = 0; i < classNames.length; i++) {
@@ -107,8 +148,27 @@ function createCards() {
     frontCard.className = 'card-front ' + classNames[i];
     var backCard = document.createElement('div');
     backCard.className = 'card-back';
-    cardContainer.append(frontCard);
     cardContainer.append(backCard);
+    cardContainer.append(frontCard);
     gameCards.append(cardContainer);
   }
+}
+
+function countdown() {
+  if (num !== 'undefined') {
+    if (num === 0) {
+      loseGame();
+    } else {
+      timeLeft.textContent = (num / 10).toFixed(1) + "s";
+      num--;
+    }
+  }
+}
+
+function loseGame() {
+  clearInterval(id);
+  timeLeft.textContent = (num / 10).toFixed(1) + "s";
+  modalOverlay.classList.remove('hidden');
+  modalContent.textContent = 'You lose!'
+  modalParagraph.textContent = 'Want to try again?';
 }
